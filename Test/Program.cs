1c4f5c7e171e0264c55bb0cc2fa9741008b93a77
A Cengic <cengica@hotmail.com>
2016-12-20 21:47:01 +0100

    Initial Commmit

---
+++ Test/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Test
+{
+	class MainClass
+	{
+		static void passed(string testName)
+		{
+			Console.WriteLine(testName + " OK ");
+		}
+
+		static void failed(string testName)
+		{
+			Console.WriteLine(testName + " FAILED ");
+
+		}
+
+		public static void Main(string[] args)
+		{
+			TopTenComponent.TopTen tt = new TopTenComponent.TopTen();
+
+			Console.WriteLine("Starting Test");
+
+			string testName;
+
+			{
+				testName = "TEST getHtmlFromUrls";
+				var result = tt.getHtmlFromUrls(new List<Uri> { new Uri("http://www.klix.ba") });
+				if (result[0].Length > 1000) passed(testName);
+				else failed(testName);
+			}
+
+			{
+				testName = "TEST removeHTMLs";
+				var result = tt.removeHTMLs(new List<String> { "<h1> TEST </h2>" });
+
+				if (result[0] == "TEST") passed(testName);
+				else failed(testName);
+			}
+
+			{
+				testName = "TEST getWordCounts";
+				var result = tt.getWordCounts(new List<String> { "juce", "danas", "sutra", "danas" } );
+
+				if (result["danas"] == 2) passed(testName);
+				else failed(testName);
+			}
+
+			{
+				{
+					testName = "TEST getWordListFromTexts";
+					var result = tt.getWordListFromTexts(new List<String> { "juce, danas i sutra" }); ;
+
+					if (result[0].Count == 4) passed(testName);
+					else failed(testName);
+				}
+
+
+				{
+					testName = "TEST getWordListFromTexts";
+					Dictionary<string, int> d1 = new Dictionary<string, int>();
+					d1.Add("danas", 2);
+					d1.Add("sutra", 1);
+					Dictionary<string, int> d2 = new Dictionary<string, int>() {
+							{ "danas", 5 },
+							{ "juce", 3 }
+						};
+
+					var result = tt.makeTop10s(new List<Dictionary<string, int>>
+						{ d1, d2 });
+
+					if (result["danas"] == 6) passed(testName);
+					else failed(testName);
+				}
+
+
+			}
+
+
+		}
+	}
+}
---
+++ TopTenComponent/TopTen.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using TopTen;
+
+namespace TopTenComponent
+{
+	public class TopTen : ITopTen
+	{
+
+		public List<String> getHtmlFromUrls(List<Uri> webSites)
+		{
+			const string noHTML = "NO_HTML";
+
+			List<String> htmlList = new List<String>();
+
+			foreach (Uri webURI in webSites)
+			{
+				WebClient wc = new WebClient();
+				string html = wc.DownloadString(webURI);
+				htmlList.Add(html);
+			}
+			return htmlList;
+		}
+
+		public List<String> removeHTMLs(List<string> htmls)
+		{
+
+			//TODO Sinisa
+			//TEst case Senad
+			// Uzeti listu htmls remove all tags
+			// if "NO_HTML" return empty string
+			return new List<String> { "Danas je lijep dan.", "text2" };
+		}
+
+		public List<List<string>> getWordListFromTexts(List<string> texts)
+		{
+
+			//TODO Senad
+			//TestCase Denis
+			// Uzeti text pretvoriti ga u listu rijeci
+			// Kreirati metod koji ce Izbrisati rijeci krace od 3 slova
+			// Kreirati metod koji ce izbaciti rijeci koje sadrze broj
+			return new List<List<String>> { new List<String> { "Danas", "je", "lijep", "dan" } };
+		}
+
+		public bool test_getWordListFromTexts()
+		{
+			var testData = new List<String> { "Danas", "je", "lijep", "dan" };
+			var methodData = getWordListFromTexts(new List<string> { "Danas je lijep dan" })[0];
+
+			return testData.SequenceEqual(methodData);
+
+			/*
+			 *  Manual comparing
+			int wordCount = testData.Count;
+			if (wordCount != methodData.Count)
+			{
+				return false;
+			}
+			for (int i = 0; i < wordCount; i++)
+			{
+				if ( testData[i] != methodData[i])
+				{
+					return false;
+				}
+			}
+			return true; ;
+			*/
+
+		}
+
+		public Dictionary<string, int> getWordCounts(List<string> rijeci)
+		{
+			/*
+			 * v1 code
+			Dictionary<string, int> results = new Dictionary<string, int>();
+			foreach (string s in rijeci)
+			{
+				if (results.ContainsKey(s))
+				{
+					results[s] += 1;
+				}
+				else
+				{
+					results[s] = 1;
+				}            
+			}
+			*/
+
+			// should be case-insensitive now
+			var results = new Dictionary<string, int>(StringComparer.CurrentCultureIgnoreCase);
+			foreach (string s in rijeci)
+			{
+				int currentCount = 0;
+				//  TryGetValue is more performant than ContainsKey followed by item access
+				results.TryGetValue(s, out currentCount);
+				currentCount++;
+				results[s] = currentCount;
+			}
+
+			//TODO Denis
+			//TODO TestCases DZenita
+			// return new Dictionary<String, int> { { "Danas", 2 }, { "dan", 1 } };
+			return results;
+		}
+		public Dictionary<String, int> makeTop10s(List<Dictionary<String, int>> top10Liste)
+		{
+			// TODO Dzenita, weighted average
+			// TODO TEsttCase Adin
+			return top10Liste[0];
+		}
+	}
+}
